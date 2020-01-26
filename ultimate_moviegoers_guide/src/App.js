9d446ba342fb86a425bce066bca2ab8103ed25e6
rootedPs1 <19784698+rootedPs1@users.noreply.github.com>
2020-01-26 16:33:34 -0500

    Added a state property component

--- ultimate_moviegoers_guide/src/App.js
+++ ultimate_moviegoers_guide/src/App.js
@@ -3,7 +3,7 @@ import './App.css';
 
 import MovieCard from './components/MovieCard'
 
-const movies = [
+const originalMovies = [
   {id: 1, title: 'Star Wars'},
   {id: 2, title: 'Star Trek'},
   {id: 3, title: 'Stargate'}
@@ -11,12 +11,18 @@ const movies = [
 
 
 class App extends Component {
+  state = {movies: []}
+
+  componentDidMount(){
+    setTimeout(() => this.setState({movies: originalMovies}), 5000);
+  }
+
   render() {
+    const {movies} = this.state;
+
     return (
       <div className="App">
         {movies.map(movie => <MovieCard key={movie.id} movie={movie} />)}
-
-
       </div>
     );
   }
