53b5e150042202e8e40936f866f85c56e0cca249
rootedPs1 <19784698+rootedPs1@users.noreply.github.com>
2020-01-26 17:23:42 -0500

    Learning AJAX

--- ultimate_moviegoers_guide/src/App.js
+++ ultimate_moviegoers_guide/src/App.js
@@ -1,6 +1,6 @@
 import React, {Component} from 'react';
 import './App.css';
-
+//import apiKey from './apiKey';
 import MovieCard from './components/MovieCard'
 
 const originalMovies = [
@@ -9,12 +9,15 @@ const originalMovies = [
   {id: 3, title: 'Stargate'}
 ];
 
-
 class App extends Component {
-  state = {movies: []}
+  state = { movies: [] };
 
-  componentDidMount(){
-    setTimeout(() => this.setState({movies: originalMovies}), 5000);
+  async componentDidMount(){
+    //setTimeout(() => this.setState({movies: originalMovies}), 5000);
+    const response = await fetch(
+      'https://api.themoviedb.org/3/movie/top_rated?api_key=78ec6b1435aa01e301624172e5d69b4f&language=en-US&page=1');
+    const json = await response.json();
+    this.setState({ movies:json.results });
   }
 
   render() {
---
+++ ultimate_moviegoers_guide/src/apiKey.js
@@ -0,0 +1,1 @@
+export default '78ec6b1435aa01e301624172e5d69b4f';
