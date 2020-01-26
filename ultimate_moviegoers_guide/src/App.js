9bdb82428e25ab28da4374081f3b8460cb92562e
rootedPs1 <19784698+rootedPs1@users.noreply.github.com>
2020-01-26 16:03:21 -0500

    create MovieCard component and display on web page

--- ultimate_moviegoers_guide/src/App.js
+++ ultimate_moviegoers_guide/src/App.js
@@ -1,26 +1,30 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import MovieCard from './components/MovieCard'
+
+const movies = [
+  {id: 1, title: 'Star Wars'},
+  {id: 2, title: 'Star Trek'},
+  {id: 3, title: 'Stargate'}
+];
+
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        {movies.map(movie => <MovieCard key={movie.id} movie={movie} />)}
+
+
+      </div>
+    );
+  }
 }
 
 export default App;
+
+
+/*Old Code*/
+
+// <MovieCard movie={movies[0]}/>
