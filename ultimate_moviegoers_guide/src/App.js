6f2fd127a3a9c7b6a09b77a1136ce8a381348519
rootedPs1 <19784698+rootedPs1@users.noreply.github.com>
2020-01-29 18:41:55 -0500

    Developing the app bar - BROKEN && a few other files to add to repo

--- ultimate_moviegoers_guide/src/App.js
+++ ultimate_moviegoers_guide/src/App.js
@@ -1,7 +1,29 @@
 import React, {Component} from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import Button from '@material-ui/core/Button';
+import IconButton from '@material-ui/core/IconButton';
+import MenuIcon from '@material-ui/icons/Menu';
+
 import './App.css';
-//import apiKey from './apiKey';
+import apiKey from './apiKey';
 import MovieCard from './components/MovieCard'
+
+const useStyles = makeStyles(theme => ({
+  root: {
+    flexGrow: 1,
+  },
+  menuButton: {
+    marginRight: theme.spacing(2),
+  },
+  title: {
+    flexGrow: 1,
+  },
+}));
+
+
 
 const originalMovies = [
   {id: 1, title: 'Star Wars'},
@@ -20,18 +42,37 @@ class App extends Component {
     this.setState({ movies:json.results });
   }
 
+
+
+//Now PLaying:  https://api.themoviedb.org/3/movie/now_playing?api_key=78ec6b1435aa01e301624172e5d69b4f&language=en-US&page=1
+//Popular:  https://api.themoviedb.org/3/movie/popular?api_key=78ec6b1435aa01e301624172e5d69b4f&language=en-US&page=1
   render() {
     const {movies} = this.state;
+    const classes = useStyles();
 
     return (
-      <div className="App">
-        {movies.map(movie => <MovieCard key={movie.id} movie={movie} />)}
+      <div>
+        <AppBar position="static">
+          <Toolbar>
+            <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu">
+              <MenuIcon />
+            </IconButton>
+            <Typography variant="h6" className={classes.title}>
+              Something Here
+            </Typography>
+            <Button color="inherit">Login</Button>
+          </Toolbar>
+        </AppBar>
+
+        <div className="App">
+          {movies.map(movie => <MovieCard key={movie.id} movie={movie} />)}
+        </div>
       </div>
     );
   }
 }
 
-export default App;
+export default makeStyles(useStyles)(App);
 
 
 /*Old Code*/
